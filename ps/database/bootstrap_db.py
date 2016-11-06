7a4766fde57b0e489577aff210833b888901d74b
Jason LeCount <jlecount@sungevity.com>
2016-11-06 11:24:00 -0800

    more.  fixes and users.

--- ps/database/bootstrap_db.py
+++ ps/database/bootstrap_db.py
@@ -27,14 +27,11 @@ con.execute("CREATE TABLE user(id INTEGER PRIMARY KEY, "
             "username char(100) NOT NULL, "
             "password char(100) NOT NULL, "
             "bank_account_balance_dollars INTEGER, "
-            "status char(100) NOT NULL)")
+            "customer_status char(100))")
 
 
 # PET
 #
-con.execute("DROP TABLE IF EXISTS pet_status")
-con.execute("DROP TABLE IF EXISTS user_status")
-
 con.execute("DROP TABLE IF EXISTS pet")
 con.execute("CREATE TABLE pet( id INTEGER PRIMARY KEY, name char(100) NOT NULL, added_at DATETIME, last_modified_by INTEGER, pet_type char(100) NOT NULL, pet_status char(100) NOT NULL, cost INTEGER, FOREIGN KEY(last_modified_by) REFERENCES user(id))")
 con.execute("INSERT into pet(name, pet_type, pet_status, cost) values ('Fluffy', 'Cat', 'for sale', 20)")
@@ -47,12 +44,30 @@ con.execute("INSERT into pet(name, pet_type, pet_status, cost) values ('Snagglet
 con.execute("DROP TABLE IF EXISTS store_order")
 con.execute("CREATE TABLE store_order( id INTEGER PRIMARY KEY AUTOINCREMENT, pet_id INTEGER, user_id INTEGER, status char(100) NOT NULL, is_complete INTEGER NOT NULL, FOREIGN KEY(pet_id) REFERENCES pet(id), FOREIGN KEY(user_id) REFERENCES user(id))")
 
-con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, status) values (?, ?, ?, ?, ?)",  ("admin", "jlecount@sungevity.com", "somepass", 0, "non_customer"));
-con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, status) values (?, ?, ?, ?, ?)",  ("admin", "gsandhu@sungevity.com", "sungevity", 0, "non_customer"));
-con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, status) values (?, ?, ?, ?, ?)",  ("admin", "mbrewer@sungevity.com", "somepass", 0, "non_customer"));
-con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, status) values (?, ?, ?, ?, ?)",  ("admin", "kgavar@sungevity.com", "somepass", 0, "non_customer"));
-con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, status) values (?, ?, ?, ?, ?)",  ("admin", "bgordon@sungevity.com", "somepass", 0, "non_customer"));
-con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, status) values (?, ?, ?, ?, ?)",  ("admin", "hkhan@sungevity.com", "sungevity", 0, "non_customer"));
-con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, status) values (?, ?, ?, ?, ?)",  ("admin", "efattig@sungevity.com", "sungevity", 0, "non_customer"));
+# ADMIN USERS
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "jlecount@sungevity.com", "7655ab", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "gsandhu@sungevity.com", "434fda8", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "mbrewer@sungevity.com", "343a9", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "kwu@sungevity.com", "af7dakk", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "kgavar@sungevity.com", "432235", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "bgordon@sungevity.com", "adsafl33", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "hkhan@sungevity.com", "9afjjj", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "efattig@sungevity.com", "89adfa", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "jtanner@sungevity.com", "34300jk", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "spolonsky@sungevity.com", "eruagg", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "biamranond@sungevity.com", "pouief", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "manantha@sungevity.com", "aPPPa", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "rvandijk@sungevity.com", "n343y", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "mcalderaz@sungevity.com", "<>43<>", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "shernandez@sungevity.com", "55443HI", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "skutty@sungevity.com", "iafsd34", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "rkaul@sungevity.com", "ytrfg89", 0));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars) values (?, ?, ?, ?)",  ("admin", "sakerson@sungevity.com", "67564a", 0));
+
+# CUSTOMERS
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, customer_status) values (?, ?, ?, ?, ?)",  ("customer", "joebob@bob.com", "strongpass", 10, "registered"));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, customer_status) values (?, ?, ?, ?, ?)",  ("customer", "sally@gmail.com", "nobodywillguess", 75, "registered"));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, customer_status) values (?, ?, ?, ?, ?)",  ("customer", "dave@example.org", "nobodywillguess", 100, "registered"));
+con.execute("INSERT into user(role, username, password, bank_account_balance_dollars, customer_status) values (?, ?, ?, ?, ?)",  ("customer", "fred@yahoo.com", "petlover", 50, "registered"));
 
 con.commit()
